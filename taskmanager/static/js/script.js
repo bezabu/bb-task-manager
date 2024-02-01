bd9c5ef7f8d7a479b3d7394b5945399166828bcd
bezabu <>
2024-02-01 15:41:05 +0000

    Add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     //sidenav initialization
-    var sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    //datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
